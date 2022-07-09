fe120ef609392981d9abe324ae6fac8971f06126
Lucas Franzolin <lucasfranzolin@gmail.com>
2022-07-09 17:44:36 -0300

    tested save-customer usecase

--- src/app/usecases/save-customer/usecase.spec.ts
+++ src/app/usecases/save-customer/usecase.spec.ts
@@ -1,14 +1,24 @@
+import dcopy from 'deep-copy';
+import { Customer } from '../../../core/entities/Customer';
 import { InMemoryCustomersRepository } from '../../../test-utils/repositories/InMemoryCustomersRepository';
 import { IPhoneProvider } from '../../providers/interfaces/IPhoneProvider';
 import { PhoneProvider } from '../../providers/PhoneProvider';
 import { SaveCustomerUseCase } from './usecase';
 
-describe('create-customer', () => {
+const mockData = {
+    email: 'email@test.com',
+    fullName: 'tester',
+    phone: '19988803452',
+};
+
+describe('save-customer', () => {
+    let data: any;
     let customerRepository: InMemoryCustomersRepository;
     let phoneProvider: IPhoneProvider;
     let sut: SaveCustomerUseCase;
 
     beforeEach(() => {
+        data = dcopy(mockData);
         customerRepository = new InMemoryCustomersRepository();
         phoneProvider = new PhoneProvider();
         sut = new SaveCustomerUseCase(customerRepository, phoneProvider);
@@ -16,5 +26,27 @@ describe('create-customer', () => {
 
     afterEach(() => {
         customerRepository.items = [];
+    });
+
+    it('should throw if phone is invalid', async () => {
+        data.phone = '11111111111111';
+        await expect(sut.execute(data)).rejects.toThrow();
+    });
+
+    it('should throw if email is already being used', async () => {
+        const customer = Customer.create({
+            email: data.email,
+            fullName: 'test',
+            phone: '01234567891',
+        });
+        expect(customerRepository.items.length).toBe(0);
+        customerRepository.items.push(customer);
+        expect(customerRepository.items.length).toBe(1);
+        await expect(sut.execute(data)).rejects.toThrow();
+    });
+    it('should save new customer', async () => {
+        expect(customerRepository.items.length).toBe(0);
+        await sut.execute(data);
+        expect(customerRepository.items.length).toBe(1);
     });
 });
