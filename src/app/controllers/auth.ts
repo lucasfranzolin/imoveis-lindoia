f05b87555fecef285f5a7464a63811ae8d1f2c19
Lucas Franzolin <lucasfranzolin@gmail.com>
2022-08-13 18:04:52 -0300

    account verification by env

--- src/app/controllers/auth.ts
+++ src/app/controllers/auth.ts
@@ -6,6 +6,7 @@ import { signUpUseCase } from '../usecases/sign-up';
 import { signOutUseCase } from '../usecases/sign-out';
 import { verifyConfirmationTokenUseCase } from '../usecases/verify-confirmation-token';
 import path from 'path';
+import { config } from '../../config/config';
 
 export async function signUp(
     req: Request,
@@ -66,8 +67,8 @@ export async function verify(
 ): Promise<void> {
     try {
         const { confirmationToken } = req.params;
-        // await verifyConfirmationTokenUseCase.execute({ confirmationToken });
-        const fileName = 'account-verified.html';
+        await verifyConfirmationTokenUseCase.execute({ confirmationToken });
+        const fileName = `account-verified.${config.env}.html`;
         const filePath = path.join(__dirname, `../static/${fileName}`);
         res.sendFile(filePath);
     } catch (err) {
