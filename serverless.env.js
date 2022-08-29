ee46532ccaf631002c584e863feb4d6a752dc724
Lucas Franzolin <lucasfranzolin@gmail.com>
2022-08-29 08:53:02 -0300

    migrate env to params

--- serverless.env.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-module.exports = async ({ resolveVariable }) => {
-    const NODE_ENV = await resolveVariable('sls:stage');
-    const envVars = dotenv.config({ path: `.env.${NODE_ENV}` }).parsed;
-    Object.assign(envVars, { NODE_ENV });
-    return envVars;
-};
